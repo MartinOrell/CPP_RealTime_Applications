b673d78d425bc7f048cbeb64102e8b55e7404610
Martin Orell <martin0stars@gmail.com>
2024-08-07 10:41:44 +0200

    Moved helloMessage projects

--- tutorialProjects/helloMessage/backFourthMessages/usingInvoke/basic/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Message.h"
-#include "MessageHandler.h"
-#include "CapsuleRunner.h"
-
-int main(){
-    MessageHandler<Message> messageHandler;
-    CapsuleRunner capsuleRunner(&messageHandler);
-    capsuleRunner.run();
-}
