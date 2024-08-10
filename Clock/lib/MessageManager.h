bd3dcddab3831fc16de64dbfe0eabef27382e59a
Martin Orell <martin0stars@gmail.com>
2024-08-10 07:41:22 +0200

    update Clock to lib 1.3

--- Clock/lib/MessageManager.h
+++ Clock/lib/MessageManager.h
@@ -7,6 +7,7 @@ class MessageManager{
     public:
         MessageManager(bool allowCrossInvoke = false);
         void addCapsuleRunnerPtr(CapsuleRunner* capsuleRunner);
+        void replaceCapsuleRunnerPtr(CapsuleRunner* to, CapsuleRunner* from);
         void sendMessage(SendMessage sendMessage);
         void mergeOrSendMessage(SendMessage);
         Message invokeMessage(SendMessage);
