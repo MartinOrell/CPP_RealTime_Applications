c720e32308f856cfe19364c5bbc2507a37ee693a
Martin Orell <martin0stars@gmail.com>
2024-08-25 08:47:04 +0200

    update to myRealTime 1.2

--- tutorialProjects/helloTimer/SeparateThread/caps/HelloTimer_Capsule.h
+++ tutorialProjects/helloTimer/SeparateThread/caps/HelloTimer_Capsule.h
@@ -12,9 +12,9 @@ namespace mrt{
 class HelloTimer_Capsule: public mrt::Capsule{
     public:
         HelloTimer_Capsule(int id, mrt::CapsuleRunner* capsuleRunnerPtr, mrt::CapsuleRunner* timerRunnerPtr);
-        int getId();
-        void start();
-        void handleMessage(const mrt::Message& message);
+        int getId() override;
+        void start() override;
+        void handleMessage(const mrt::Message& message) override;
     private:
         void handleTimeout(const mrt::TimeoutMessage& message);
 
